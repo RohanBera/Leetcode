e3526f5a013eef8193184d1a21b2032151a63b25
RohanBera <berarohan88@gmail.com>
2021-02-23 22:44:38 +0530

    9 - Palindrome number - 35% / 46%

---
+++ random/9_palindromeNumber.java
@@ -0,0 +1,16 @@
+// 36% / 46% 
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0)
+            return false;
+
+        String s = String.valueOf(x);
+
+        for (int i = 0, j = s.length() - 1; i <= j; i++, j--)
+            if (s.charAt(i) != s.charAt(j))
+                return false;
+
+        return true;
+    }
+}
