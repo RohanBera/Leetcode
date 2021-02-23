5900801f04531a9c34f8398a9efd566d4adb30cb
RohanBera <berarohan88@gmail.com>
2021-02-23 23:13:43 +0530

    14 - longest common prefix - 20% / 21%

---
+++ random/14_longestCommonPrefix.java
@@ -0,0 +1,22 @@
+// 20% / 21%
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0)
+            return "";
+
+        String sol = "";
+
+        for (int i = 0; i < strs[0].length(); i++) {
+            char hm = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (i == strs[j].length() || strs[j].charAt(i) != hm)
+                    return sol;
+            }
+
+            sol += hm;
+        }
+
+        return sol;
+    }
+}
